b03cd2b120ca6a55a7d3ab3c2d0515e72c632c33
Charles <vectorshinz@gmail.com>
2021-01-30 21:44:37 +0800

    added attribs

--- dist/P2.php
+++ dist/P2.php
@@ -12,7 +12,23 @@
     <main id="main-content">
         <div class="table-container">
             <h1 class="bg-title">Multiplication Table</h1>
+            <?php
+            echo "<table>";
+            for ($i = 0, $c = 0; $i <= 10; $i++) {
+                echo "<tr>";
+                for ($j = 0; $j <= 10; $j++) {
+                    if ($c % 2 == 0) {
+                        echo "<td class='s1'>" . $i * $j . "</td>";
+                    } else {
+                        echo "<td class='s2'>" . $i * $j . "</td>";
+                    }
+                    $c++;
+                }
+                echo "</tr>";
+            }
+            echo "<table>";
 
+            ?>
         </div>
     </main>
 </body>
