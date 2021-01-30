45ba43c630934376609462fca63637403903a486
Charles <vectorshinz@gmail.com>
2021-01-30 19:40:31 +0800

    first commit

---
+++ dist/P2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/P2.css">
+    <title>Problem 2</title>
+</head>
+
+<body>
+    <main id="main-content">
+        <div class="table-container">
+            <h1 class="bg-title">Multiplication Table</h1>
+
+        </div>
+    </main>
+</body>
+
+</html>
